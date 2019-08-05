943eb556b0b1dcfbbd4e1793edb1e22021e0e391
ZhiangChen <zxc251@case.edu>
2019-08-04 20:59:34 -0700

    added headed adjustment

--- terrain_following/src/mavros_vel_controller.py
+++ terrain_following/src/mavros_vel_controller.py
@@ -12,6 +12,7 @@ from pymavlink import mavutil
 from std_msgs.msg import Header
 from threading import Thread
 from tf.transformations import quaternion_from_euler
+from tf.transformations import euler_from_quaternion
 from mavros_controller import Mavros_Controller as MController
 
 import actionlib
@@ -19,19 +20,25 @@ import terrain_following.msg
 
 
 class Mavros_Velocity_Controller(MController):
-    def __init__(self, radius=0.2, K_xy=0.3, K_z=1, K=0.3, step_size=0.3):
+    def __init__(self, radius=0.1, step_size=0.3):
         super(Mavros_Velocity_Controller, self).__init__()
 
         # controller parameters
         self.radius = radius
-        self.K_I = 0.3
-        self.K_xy = 0.5 # K_xy
-        self.K_z = K_z
-        self.K = K
+        self.K_xy_I = 0.3  # xy integral gain
+        self.K_xy_P = 0.5  # xy proportional gain
+        self.K_z_P = 1.0  # z proportional gain
+        self.K_P = 0.3  # proportional gain for approaching stage or takeoff
+        self.K_I = 0.1  # integral gain for approaching stage or takeoff
+        self.K_angle_P = 0.1
         self.step_size = step_size
         self._xy_err_old = np.zeros(2)
+        self._xyz_err_old = np.zeros(3)
 
         self._takeoff = False
+        self._adjust_head = False
+        self._turning_direction = False
+        self._old_position = np.zeros(3)
 
         self._as = actionlib.SimpleActionServer("waypoints_mission", terrain_following.msg.waypointsAction,
                                                 execute_cb=self.execute_cb, auto_start=False)
@@ -60,7 +67,7 @@ class Mavros_Velocity_Controller(MController):
         except:
             rospy.logerr("Invalid waypoints")
 
-        self.__posctl(waypoints)
+        self.__velctl(waypoints)
 
         #feedback = terrain_following.msg.waypointsFeedback()
         result = terrain_following.msg.waypointsResult()
@@ -78,16 +85,44 @@ class Mavros_Velocity_Controller(MController):
         while not rospy.is_shutdown():
             desire_xyz = np.array((self.pos.pose.position.x, self.pos.pose.position.y, self.pos.pose.position.z))
             current_xyz = np.array((self.local_position.pose.position.x, self.local_position.pose.position.y, self.local_position.pose.position.z))
+            current_angle_z = np.array(euler_from_quaternion((self.local_position.pose.orientation.x, self.local_position.pose.orientation.y, self.local_position.pose.orientation.z, self.local_position.pose.orientation.w)))[2]
             err = desire_xyz - current_xyz
+            desire_angle_z = math.atan2(err[1], err[0])
+            err_angle_z = desire_angle_z - current_angle_z
             if not self._takeoff:
                 control_err = err
-                vel = control_err * self.K
+                vel = control_err * self.K_P + self._xyz_err_old * self.K_I
                 vel = np.max((vel, vel_min), axis=0)
                 vel = np.min((vel, vel_max), axis=0)
                 self.vel.twist.linear.x, self.vel.twist.linear.y, self.vel.twist.linear.z = vel[0], vel[1], vel[2]
                 self.vel.header.stamp = rospy.Time.now()
                 self.vel_setpoint_pub.publish(self.vel)
+                self._xyz_err_old = control_err
                 rate.sleep()
+
+            elif not self._adjust_head :
+                control_err = self._old_position - current_xyz
+                vel = control_err * self.K_P + self._xyz_err_old * self.K_I
+                self.vel.twist.linear.x, self.vel.twist.linear.y, self.vel.twist.linear.z = vel[0], vel[1], vel[2]
+                if not self._turning_direction:
+                    if err_angle_z > 0:
+                        self._turning_direction = 1
+                    else:
+                        self._turning_direction = -1
+                angle_vel_z = abs(err_angle_z * self.K_angle_P) * self._turning_direction
+                angle_vel_z = np.max((angle_vel_z, -0.2), axis=0)
+                angle_vel_z = np.min((angle_vel_z, 0.2), axis=0)
+                self.vel.twist.angular.z = angle_vel_z
+                self.vel.header.stamp = rospy.Time.now()
+                self.vel_setpoint_pub.publish(self.vel)
+                self._xyz_err_old = control_err
+                rate.sleep()
+                if abs(err_angle_z) < 0.01:
+                    self.vel.twist.angular.z = 0
+                    self._adjust_head = True
+                    self._turning_direction = False
+                    self._xyz_err_old = np.zeros(3)
+
 
             elif np.linalg.norm(err) > self.radius*4:
                 xy_err = err[:2]
@@ -105,8 +140,8 @@ class Mavros_Velocity_Controller(MController):
                         next_xyz[2] = result.z
                         if not result.got_terrain:
                             print(result)
-                z_vel = (next_xyz[2] - current_xyz[2]) * self.K_z
-                xy_vel = xy_step_size * self.K_xy + (xy_step_size + self._xy_err_old) * self.K_I
+                z_vel = (next_xyz[2] - current_xyz[2]) * self.K_z_P
+                xy_vel = xy_step_size * self.K_xy_P + (xy_step_size + self._xy_err_old) * self.K_xy_I
                 vel = np.append(xy_vel, z_vel)
                 vel = np.max((vel, vel_min), axis=0)
                 vel = np.min((vel, vel_max), axis=0)
@@ -127,14 +162,14 @@ class Mavros_Velocity_Controller(MController):
                     if type(result) is not None:
                         next_xyz[2] = result.z
 
-
                 control_err = next_xyz - current_xyz
-                vel = control_err * self.K
+                vel = control_err * self.K_P + self._xyz_err_old * self.K_I
                 vel = np.max((vel, vel_min), axis=0)
                 vel = np.min((vel, vel_max), axis=0)
                 self.vel.twist.linear.x, self.vel.twist.linear.y, self.vel.twist.linear.z = vel[0], vel[1], vel[2]
                 self.vel.header.stamp = rospy.Time.now()
                 self.vel_setpoint_pub.publish(self.vel)
+                self._xyz_err_old = control_err
             else:
                 rate.sleep()
 
@@ -186,6 +221,10 @@ class Mavros_Velocity_Controller(MController):
                 rospy.loginfo("position reached | seconds: {0} of {1}".format(
                     i / loop_freq, timeout))
                 reached = True
+                self._adjust_head = False
+                self._old_position = np.array((self.pos.pose.position.x, self.pos.pose.position.y, self.pos.pose.position.z))
+                self._xy_err_old = np.zeros(2)
+                self._xyz_err_old = np.zeros(3)
                 break
 
             try:
@@ -199,7 +238,7 @@ class Mavros_Velocity_Controller(MController):
                    self.local_position.pose.position.y,
                    self.local_position.pose.position.z, timeout)))
 
-    def __posctl(self, positions):
+    def __velctl(self, positions):
         """Test offboard position control"""
 
         # make sure the simulation is ready to start the mission
@@ -216,7 +255,7 @@ class Mavros_Velocity_Controller(MController):
         self.relative_height = positions[0][2]
         self._goal.relative_height = positions[0][2]
         for i in xrange(len(positions)):
-            if i>0:
+            if i > 0:
                 self._takeoff = True
             self.reach_waypoint(positions[i][0], positions[i][1],
                                 positions[i][2], 600)
